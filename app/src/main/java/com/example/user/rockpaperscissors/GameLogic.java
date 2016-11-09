0a442341eb01cc5b613e3ab9cbe81ca3f5e41fc2
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-11-09 12:14:40 +0000

    adding WIN_AGAINST HashMap

--- app/src/main/java/com/example/user/rockpaperscissors/GameLogic.java
+++ app/src/main/java/com/example/user/rockpaperscissors/GameLogic.java
@@ -1,9 +1,26 @@
 package com.example.user.rockpaperscissors;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by user on 09/11/2016.
  */
 
 public class GameLogic {
+
+    enum Hand {
+        ROCK,
+        PAPER,
+        SCISSORS
+    }
+
+    private static final HashMap<Hand, Hand> WIN_AGAINST;
+    static {
+        WIN_AGAINST = new HashMap<>();
+        WIN_AGAINST.put( Hand.ROCK, Hand.SCISSORS );
+        WIN_AGAINST.put( Hand.PAPER, Hand.ROCK );
+        WIN_AGAINST.put( Hand.SCISSORS, Hand.PAPER );
+    }
 }
 
